8d5a361b535cbf28402d28971a6d714ad3d24561
vgaidarji <veaceslav.gaidarji@gmail.com>
2016-10-08 14:48:42 +0300

    Finish lifx notifier step

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Step for showing status using your [LIFX](http://www.lifx.com) bulb during builds.
 
+This step uses [official LIFX API](https://api.developer.lifx.com/docs/introduction) which requires OAuth2 Access token.
+[Here](https://api.developer.lifx.com/docs/authentication) are the steps how to generate the access token.
+
 ## How to use this Step
 
 Can be run directly with the [bitrise CLI](https://github.com/bitrise-io/bitrise),
--- step.sh
+++ step.sh
@@ -1,21 +1,58 @@
 #!/bin/bash
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+# defaults
+DEFAULT_COLOR_BUILD_STARTED="yellow"
+DEFAULT_COLOR_BUILD_SUCCESS="green"
+DEFAULT_COLOR_BUILD_FAILURE="red"
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-#  envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+# use custom colors if set (not empty string)
+is_custom_color=false
+if [ -n "${color_build_started_custom}" ] ; then
+    color_build_started=${color_build_started_custom}
+    is_custom_color=true
+fi
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+if [ -n "${color_build_failure_custom}" ] ; then
+    color_build_failure=${color_build_failure_custom}
+    is_custom_color=true
+fi
+
+if [ -n "${color_build_success_custom}" ] ; then
+    color_build_success=${color_build_success_custom}
+    is_custom_color=true
+fi
+
+# use default colors if no colors provided (empty string)
+if [ -z "${color_build_started}" ] ; then
+    color_build_started=${DEFAULT_COLOR_BUILD_STARTED}
+fi
+
+if [ -z "${color_build_failure}" ] ; then
+    color_build_failure=${DEFAULT_COLOR_BUILD_FAILURE}
+fi
+
+if [ -z “${color_build_success}” ] ; then
+    color_build_success=${DEFAULT_COLOR_BUILD_SUCCESS}
+fi
+
+# check which color to use
+if [[ "${workflow_start}" == "yes" ]] ; then
+    color=${color_build_started}
+else
+    if [[ "${BITRISE_BUILD_STATUS}" == "0" ]] ; then
+        color=${color_build_success}
+    else
+        color=${color_build_failure}
+    fi
+fi
+
+if ${is_custom_color}; then
+    body="{\"color\":\"#${color}\",\"power\":\"on\"}"
+else
+    body="{\"color\":\"${color}\",\"power\":\"on\"}"
+fi
+
+# change bulb color via API
+curl -s -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer ${auth_token}" -d ${body} "https://api.lifx.com/v1/lights/label:${bulb_label}/state" #> /dev/null
+return_code=$?
+exit ${return_code}
