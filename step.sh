cde49f0685b9be89be87605c4d6708dd6f3022f4
vgaidarji <veaceslav.gaidarji@gmail.com>
2016-10-09 13:48:40 +0300

    Remove redundant step parameters
    Closes #3

--- step.sh
+++ step.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 # defaults
-DEFAULT_COLOR_BUILD_STARTED="yellow"
 DEFAULT_COLOR_BUILD_SUCCESS="green"
 DEFAULT_COLOR_BUILD_FAILURE="red"
 
 # use custom colors if set (not empty string)
 is_custom_color=false
-if [ -n "${color_build_started_custom}" ] ; then
-    color_build_started=${color_build_started_custom}
-    is_custom_color=true
-fi
-
 if [ -n "${color_build_failure_custom}" ] ; then
     color_build_failure=${color_build_failure_custom}
     is_custom_color=true
@@ -23,10 +17,6 @@ if [ -n "${color_build_success_custom}" ] ; then
 fi
 
 # use default colors if no colors provided (empty string)
-if [ -z "${color_build_started}" ] ; then
-    color_build_started=${DEFAULT_COLOR_BUILD_STARTED}
-fi
-
 if [ -z "${color_build_failure}" ] ; then
     color_build_failure=${DEFAULT_COLOR_BUILD_FAILURE}
 fi
@@ -36,14 +26,10 @@ if [ -z “${color_build_success}” ] ; then
 fi
 
 # check which color to use
-if [[ "${workflow_start}" == "yes" ]] ; then
-    color=${color_build_started}
+if [[ "${BITRISE_BUILD_STATUS}" == "0" ]] ; then
+    color=${color_build_success}
 else
-    if [[ "${BITRISE_BUILD_STATUS}" == "0" ]] ; then
-        color=${color_build_success}
-    else
-        color=${color_build_failure}
-    fi
+    color=${color_build_failure}
 fi
 
 if ${is_custom_color}; then
